8ffd2db34294a4338d23f6ddbd40519fb5c32428
s3819660 <56669093+s3819660@users.noreply.github.com>
2022-03-12 21:26:34 +0700

    setup rss reader

--- app/src/main/java/com/example/rss_reader/MainActivity.java
+++ app/src/main/java/com/example/rss_reader/MainActivity.java
@@ -2,13 +2,144 @@ package com.example.rss_reader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    ListView listView;
+    ArrayList<String> titles;
+    ArrayList<String> descriptions;
+    ArrayList<String> links;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titles = new ArrayList<>();
+        descriptions = new ArrayList<>();
+        links = new ArrayList<>();
+
+        // Get views
+        getViews();
+    }
+
+    private void getViews() {
+        listView = findViewById(R.id.list_view);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Uri uri = Uri.parse(links.get(i));
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
+        new ProcessInBackground().execute();
+    }
+
+    private InputStream getInputStream(URL url) {
+        try {
+            return url.openConnection().getInputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public class ProcessInBackground extends AsyncTask<Integer, Void, Exception> {
+
+        ProgressDialog progressDialog = new ProgressDialog(MainActivity.this);
+        Exception exception = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog.setMessage("Loading feed....");
+            progressDialog.show();
+        }
+
+        @Override
+        protected Exception doInBackground(Integer... integers) {
+
+            try {
+                URL url = new URL("https://vnexpress.net/rss/the-gioi.rss");
+
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+
+                // Don't provide support for xml namespace
+                factory.setNamespaceAware(false);
+
+                XmlPullParser xpp = factory.newPullParser();
+
+                // encoding = UTF_8
+                xpp.setInput(getInputStream(url), "UTF_8");
+
+                boolean insideItem = false;
+
+                // Which tag/where we are in the document
+                int eventType = xpp.getEventType();
+
+                while (eventType != XmlPullParser.END_DOCUMENT) {
+                    if (eventType == XmlPullParser.START_TAG) {
+                        if (xpp.getName().equalsIgnoreCase("item")) {
+                            insideItem = true;
+                        } else if (xpp.getName().equalsIgnoreCase("title")) {
+                            if (insideItem) {
+                                titles.add(xpp.nextText());
+                            }
+                        } else if (xpp.getName().equalsIgnoreCase("description")) {
+                            if (insideItem) {
+                                descriptions.add(xpp.nextText());
+                            }
+                        } else if (xpp.getName().equalsIgnoreCase("link")) {
+                            if (insideItem) {
+                                links.add(xpp.nextText());
+                            }
+                        }
+                    } else if (eventType == XmlPullParser.END_DOCUMENT && xpp.getName().equalsIgnoreCase("item")) {
+                        insideItem = false;
+                    }
+
+                    eventType = xpp.next();
+                }
+            } catch (MalformedURLException e) {
+                exception = e;
+            } catch (XmlPullParserException e) {
+                exception = e;
+            } catch (IOException e) {
+                exception = e;
+            }
+
+            return exception;
+        }
+
+        @Override
+        protected void onPostExecute(Exception e) {
+            super.onPostExecute(e);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(MainActivity.this,
+                    android.R.layout.simple_list_item_1, titles);
+            listView.setAdapter(adapter);
+            progressDialog.dismiss();
+        }
     }
 }
